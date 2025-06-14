9011ed43ae2d7f9eb5bf228eb66e0cecb935eb7a
guiaf04 <guilhermearaujo@alu.ufc.br>
2025-06-14 00:16:08 -0300

    adicionando sincronização no servidor RMI para aguardar invocações de clientes

--- RMI/src/main/java/org/example/server/Server.java
+++ RMI/src/main/java/org/example/server/Server.java
@@ -6,16 +6,25 @@ import java.rmi.registry.Registry;
 
 public class Server {
   public static void main(String[] args) {
+    final Object lock = new Object();
+
     try {
       CampeonatoManager manager = new CampeonatoManagerImpl();
+
       Registry registry = LocateRegistry.createRegistry(1099);
+
       registry.bind("CampeonatoService", manager);
 
       System.out.println("Servidor RMI está pronto.");
       System.out.println("Aguardando invocações de clientes...");
 
+
+      synchronized (lock) {
+        lock.wait();
+      }
+
     } catch (Exception e) {
-      System.err.println("Exceção no servidor: " + e.toString());
+      System.err.println("Exceção no servidor: " + e);
       e.printStackTrace();
     }
   }
